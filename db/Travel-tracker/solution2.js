8f6679ee51d3429572e83b4a12d5529226a26dab
Ilya <iliadorvar@gmail.com>
2025-03-02 20:24:52 +0330

    Updating project, Adding POST route, this project is Only for Learning PostgreSQL

--- db/Travel-tracker/solution2.js
+++ db/Travel-tracker/solution2.js
@@ -1,35 +1,31 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from 'pg';
+import pg from "pg";
 
 const app = express();
-const PORT = 3000;
-
+const port = 3000;
 
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "db name",
-  password: "your pw",
+  database: "world",
+  password: "123456",
   port: 5432,
 });
 db.connect();
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.set('view engine', 'ejs');
-
 
 async function checkVisisted() {
-  const result = await db.query("SELECT country FROM user_countries");
+  const result = await db.query("SELECT country_code FROM visited_countries");
+
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
   return countries;
-};
-
+}
 
 // GET home page
 app.get("/", async (req, res) => {
@@ -37,26 +33,26 @@ app.get("/", async (req, res) => {
   res.render("index.ejs", { countries: countries, total: countries.length });
 });
 
-
 //INSERT new country
 app.post("/add", async (req, res) => {
-  const input = req.body.country;
+  const input = req.body["country"];
+
   const result = await db.query(
-    "SELECT country_code FROM visited_country WHERE country_name = $1", [input]
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
   );
-  console.log(result);
 
   if (result.rows.length !== 0) {
     const data = result.rows[0];
     const countryCode = data.country_code;
-    await db.query("INSERT INTO user_countries (country) VALUES ($1)", [
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
       countryCode,
     ]);
     res.redirect("/");
   }
 });
 
-
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
